7a5682f6290cba65285e7c9fe29c34c36cf5b342
xpyx <pystynen@gmail.com>
2021-03-24 22:34:19 +0200

    MQTT messages received

--- NokiaHslVisualisation/app/src/main/java/com/xpyx/nokiahslvisualisation/fragments/home/HomeFragment.kt
+++ NokiaHslVisualisation/app/src/main/java/com/xpyx/nokiahslvisualisation/fragments/home/HomeFragment.kt
@@ -16,13 +16,18 @@ import com.xpyx.nokiahslvisualisation.GetAlertsQuery
 import com.xpyx.nokiahslvisualisation.R
 import com.xpyx.nokiahslvisualisation.networking.apolloClient.ApolloClient
 import android.text.method.ScrollingMovementMethod
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.eclipse.paho.android.service.MqttAndroidClient
 import org.eclipse.paho.client.mqttv3.*
+import kotlin.properties.Delegates
 
 class HomeFragment : Fragment() {
 
     private lateinit var mqttAndroidClient: MqttAndroidClient
-
+    private val scope = CoroutineScope(Dispatchers.IO)
+    private var counter: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -62,10 +67,18 @@ class HomeFragment : Fragment() {
 
         // initialize 'num msgs received' field in the view
         val textViewNumMsgs = view.findViewById<TextView>(R.id.textViewNumMsgs)
-        textViewNumMsgs.text = "0"
+        textViewNumMsgs.text = counter.toString()
 
         btnPositions.setOnClickListener{
-//            mqttClient.subscribe("/hfp/v2/journey/ongoing/vp/+/+/+/+/+/+/+/+/0/#")
+            scope.launch {
+                subscribe("/hfp/v2/journey/ongoing/vp/+/+/+/+/+/+/+/+/0/#")
+                receiveMessages()
+
+            }
+
+
+
+
         }
         return view
     }
@@ -91,5 +104,66 @@ class HomeFragment : Fragment() {
             // Give your callback on connection failure here
             e.printStackTrace()
         }
+    }
+
+
+    fun subscribe(topic: String) {
+        val qos = 2 // Mention your qos value
+        try {
+            mqttAndroidClient.subscribe(topic, qos, null, object : IMqttActionListener {
+                override fun onSuccess(asyncActionToken: IMqttToken) {
+                    // Give your callback on Subscription here
+                    Log.i("Connection", "subscribe success ")
+                }
+                override fun onFailure(
+                    asyncActionToken: IMqttToken,
+                    exception: Throwable
+                ) {
+                    // Give your subscription failure callback here
+                    Log.i("Connection", "subscribe failure")
+
+                }
+            })
+        } catch (e: MqttException) {
+            // Give your subscription failure callback here
+        }
+    }
+
+    fun receiveMessages() {
+
+        mqttAndroidClient.setCallback(object : MqttCallback {
+            override fun connectionLost(cause: Throwable) {
+                //connectionStatus = false
+                // Give your callback on failure here
+            }
+            override fun messageArrived(topic: String, message: MqttMessage) {
+                val textViewNumMsgs = view?.findViewById<TextView>(R.id.textViewNumMsgs)
+                val textViewMsgPayload = view?.findViewById<TextView>(R.id.textViewMsgPayload)
+                try {
+                    val data = String(message.payload, charset("UTF-8"))
+                    // data is the desired received message
+                    // Give your callback on message received here
+                    Log.d("Connection", data)
+                    counter++
+                    runOnUiThread {
+                        textViewNumMsgs?.text = "Number of MQTT messages: " + counter.toString()
+                        textViewMsgPayload?.text = data
+                    }
+
+
+                } catch (e: Exception) {
+                    // Give your callback on error here
+                }
+            }
+            override fun deliveryComplete(token: IMqttDeliveryToken) {
+                // Acknowledgement on delivery complete
+            }
+        })
+    }
+
+    fun Fragment?.runOnUiThread(action: () -> Unit) {
+        this ?: return
+        if (!isAdded) return // Fragment not attached to an Activity
+        activity?.runOnUiThread(action)
     }
 }
