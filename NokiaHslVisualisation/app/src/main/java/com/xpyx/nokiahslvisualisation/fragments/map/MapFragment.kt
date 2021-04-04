c4e572096da784f798b2f270c1da8530b3193321
mikaely <mikael.ylivaara@metropolia.fi>
2021-04-04 22:13:17 +0300

    Changed Map to osmdroid updated it to VR

--- NokiaHslVisualisation/app/src/main/java/com/xpyx/nokiahslvisualisation/fragments/map/MapFragment.kt
+++ NokiaHslVisualisation/app/src/main/java/com/xpyx/nokiahslvisualisation/fragments/map/MapFragment.kt
@@ -1,10 +1,20 @@
 package com.xpyx.nokiahslvisualisation.fragments.map
 
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.SeekBar
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.findFragment
+import com.google.ar.core.HitResult
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ViewRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.annotations.MarkerOptions
 import com.mapbox.mapboxsdk.camera.CameraPosition
@@ -15,11 +25,27 @@ import com.mapbox.mapboxsdk.maps.Style
 import com.mapbox.mapboxsdk.style.sources.RasterSource
 import com.mapbox.mapboxsdk.style.sources.TileSet
 import com.xpyx.nokiahslvisualisation.R
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
 
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(),SeekBar.OnSeekBarChangeListener {
 
     private var mapView: MapView? = null
+
+    private lateinit var arFrag: ArFragment
+    private var viewRenderable: ViewRenderable? = null
+    private var mTransparencyBar: SeekBar? = null
+    private var mHeightBar: SeekBar? = null
+    private var mWidthBar: SeekBar? = null
+
+    private lateinit var map: org.osmdroid.views.MapView
+    private var transparency = 0f
+    private var height = 900
+    private var width = 900
+    private lateinit var apa: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -29,10 +55,13 @@ class MapFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        context?.let { Mapbox.getInstance(
-            it.applicationContext,
-            getString(R.string.mapbox_access_token)
-        ) }
+
+        context?.let {
+            Mapbox.getInstance(
+                it.applicationContext,
+                getString(R.string.mapbox_access_token)
+            )
+        }
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_map, container, false)
     }
@@ -40,63 +69,109 @@ class MapFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val ctx = requireActivity().applicationContext
+        //important! set your user agent to prevent getting banned from the osm servers
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx))
+         //   activity?.setContentView(R.layout.activity_main)
 
-        mapView = view.findViewById(R.id.mapView)
-        mapView?.onCreate(savedInstanceState)
-        mapView?.getMapAsync { mapboxMap ->
-           // mapboxMap.getStyle {
-              //  mapboxMap.setStyle(Style.MAPBOX_STREETS) {
-                    // Adding a raster source layer
+        mTransparencyBar = activity?.findViewById(R.id.transparencySeekBar)
+        mTransparencyBar?.max = TRANSPARENCY_MAX
+        mTransparencyBar?.progress = 100
 
-                     mapboxMap.setStyle(Style.Builder().fromUri("asset://local_style")) {
+        mHeightBar = activity?.findViewById(R.id.heightSeekBar)
+        mHeightBar?.max = HEIGHT_MAX
+        mHeightBar?.min = HEIGHT_MIN
+        mHeightBar?.progress = 2475
 
-                    val source = RasterSource(
-                        "map",
-                        TileSet( "https://cdn.digitransit.fi/map/v1/hsl-map" + "/{z}/{x}/{y}.png","mapbox://mapid")
-                    )
-
-
-
-                    mapboxMap?.addMarker(
-                        MarkerOptions()
-                            .position(LatLng(60.444, 24.0, 1.0))
-                    )
-                    val position = CameraPosition.Builder()
-                        .target(LatLng(60.1733244, 24.9410248))
-                        .zoom(10.0)
-                        .tilt(20.0)
-                        .build()
-                    mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position), 1000)
-                    it.addSource(source)
+        mWidthBar = activity?.findViewById(R.id.widthSeekBar)
+        mWidthBar?.max = WIDTH_MAX
+        mWidthBar?.min = WIDTH_MIN
+        mWidthBar?.progress = 2475
 
 
+        arFrag = childFragmentManager.findFragmentById(
+            R.id.sceneform_fragment
+        ) as ArFragment
+
+        ViewRenderable.builder()
+            .setView(requireContext(), R.layout.mymap)
+            .build()
+            .thenAcceptAsync {
+                //load apa as container of map to get size controll
+                apa = it.view as LinearLayout
+                map = it.view.findViewById<org.osmdroid.views.MapView>(R.id.map)
+                map.setTileSource(TileSourceFactory.MAPNIK)
+                map.setBuiltInZoomControls(true)
+                map.setMultiTouchControls(true)
+                map.controller.setZoom(17.0)
+                map.controller.setCenter(GeoPoint(60.17, 24.95))
+                viewRenderable = it
+            }
+
+
+        arFrag.setOnTapArPlaneListener { hitResult: HitResult?, _, _ ->
+            if (viewRenderable == null) {
+                return@setOnTapArPlaneListener
+            }
+            //Creates a new anchor at the hit location
+            val anchor = hitResult!!.createAnchor()
+            //Creates a new anchorNode attaching it to anchor
+            val anchorNode = AnchorNode(anchor)
+            // Add anchorNode as root scene node's child
+            anchorNode.setParent(arFrag.arSceneView.scene)
+            // Can be selected, rotated...
+            val viewNode = TransformableNode(arFrag.transformationSystem)
+            // Add viewNode as anchorNode's child
+            viewNode.setParent(anchorNode)
+            viewNode.renderable = viewRenderable
+            // Sets this as the selected node in the TransformationSystem
+            mTransparencyBar?.visibility = View.VISIBLE
+            mHeightBar?.visibility = View.VISIBLE
+            mWidthBar?.visibility = View.VISIBLE
+            viewNode.select()
+            Log.e("aaa", "$mTransparencyBar")
+        }
+
+        mTransparencyBar?.setOnSeekBarChangeListener(this)
+        mHeightBar?.setOnSeekBarChangeListener(this)
+        mWidthBar?.setOnSeekBarChangeListener(this)
+
+    }
+
+    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+        when (seekBar){
+            mTransparencyBar -> {
+                Log.e("AAA", "${seekBar}")
+                transparency = progress.toFloat() / TRANSPARENCY_MAX.toFloat()
+                map.alpha = transparency
+                Log.e("aaa", "$transparency")
+            }
+            mHeightBar -> {
+                height = progress
+                var params = apa.layoutParams
+                params.height = height
+                apa.layoutParams = params
+            }
+            mWidthBar -> {
+                width = progress
+                var params = apa.layoutParams
+                params.width = width
+                apa.layoutParams = params
             }
         }
     }
-    override fun onStart() {
-        super.onStart()
-        mapView?.onStart()
-    }
 
-    override fun onResume() {
-        super.onResume()
-        mapView?.onResume()
-    }
+    override fun onStartTrackingTouch(seekBar: SeekBar?) {}
 
-    override fun onPause() {
-        super.onPause()
-        mapView?.onPause()
-    }
+    override fun onStopTrackingTouch(seekBar: SeekBar?) {}
 
-    override fun onStop() {
-        super.onStop()
-        mapView?.onStop()
+    companion object {
+        private const val TRANSPARENCY_MAX = 100
+        private const val HEIGHT_MAX = 2475
+        private const val HEIGHT_MIN = 200
+        private const val WIDTH_MAX = 2475
+        private const val WIDTH_MIN = 200
     }
-
-   /* override fun onSaveInstanceState(outState: Bundle?) {
-        super.onSaveInstanceState(outState!!)
-        mapView!!.onSaveInstanceState(outState)
-    } */
 
 
 
