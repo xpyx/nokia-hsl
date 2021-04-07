ac57199a1fcd3024b09ff014242f75de9d9b3bd9
Diakko <48632853+Diakko@users.noreply.github.com>
2021-04-07 13:53:03 +0300

    Traffic details done without map

--- NokiaHslVisualisation/app/src/main/java/com/xpyx/nokiahslvisualisation/fragments/list/TrafficListAdapter.kt
+++ NokiaHslVisualisation/app/src/main/java/com/xpyx/nokiahslvisualisation/fragments/list/TrafficListAdapter.kt
@@ -2,10 +2,12 @@ package com.xpyx.nokiahslvisualisation.fragments.list
 
 
 import android.content.Context
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.os.bundleOf
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.xpyx.nokiahslvisualisation.R
@@ -27,7 +29,10 @@ class TrafficListAdapter(private val context: Context) : RecyclerView.Adapter<Tr
         titleTextView.text = traffic.trafficItemDescriptionElement?.get(0)!!.value
         val problemTextView = holder.itemView.findViewById<TextView>(R.id.description_text_view)
         problemTextView.text = context.getString(R.string.warning_description, traffic.start_time, traffic.end_time, traffic.traffic_item_type_desc)
-        holder.itemView.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_action_list_to_action_bus))
+        val trafficId = traffic.traffic_item_id
+        val positionArgument: Bundle = bundleOf("TRAFFIC" to trafficId)
+
+        holder.itemView.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_action_list_to_action_bus, positionArgument))
 
     }
 
---
+++ NokiaHslVisualisation/app/src/main/java/com/xpyx/nokiahslvisualisation/fragments/trafficdetails/TrafficDetailsFragment.kt
@@ -0,0 +1,73 @@
+package com.xpyx.nokiahslvisualisation.fragments.trafficdetails
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
+import com.xpyx.nokiahslvisualisation.R
+import com.xpyx.nokiahslvisualisation.data.DataTrafficItem
+import com.xpyx.nokiahslvisualisation.data.TrafficItemViewModel
+import com.xpyx.nokiahslvisualisation.model.traffic.TrafficItem
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.w3c.dom.Text
+
+class TrafficDetailsFragment : Fragment() {
+    private lateinit var trafficItem: DataTrafficItem
+    private lateinit var trafficItemViewModel: TrafficItemViewModel
+    private lateinit var trafficTitleTV : TextView
+    private lateinit var trafficDescriptionTV : TextView
+    private lateinit var trafficTimeTV : TextView
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_traffic_details, container, false)
+
+        trafficItemViewModel = ViewModelProvider(this).get(TrafficItemViewModel::class.java)
+        // Set toolbar back button listener
+        val callback: OnBackPressedCallback =
+            object : OnBackPressedCallback(true /* enabled by default */) {
+                override fun handleOnBackPressed() {
+                    requireView().findNavController().navigate(R.id.action_action_bus_to_action_list)
+                }
+            }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
+
+        trafficDescriptionTV = view.findViewById(R.id.traffic_description)
+        trafficTitleTV = view.findViewById(R.id.traffic_title)
+        trafficTimeTV = view.findViewById(R.id.traffic_time)
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val toolbar = (activity as AppCompatActivity).supportActionBar
+        toolbar?.title = getString(R.string.details_view)
+
+        GlobalScope.launch(context = Dispatchers.IO) {
+            val trafficID = arguments?.getLong("TRAFFIC") ?: 0
+            trafficItem = trafficItemViewModel.getTrafficItem(trafficID)
+            Log.d("TEST", trafficItem.toString())
+
+            trafficTitleTV.text = trafficItem.traffic_item_type_desc
+            trafficTimeTV.text = getString(R.string.traffic_time, trafficItem.criticality?.description, trafficItem.start_time, trafficItem.end_time)
+            trafficDescriptionTV.text = trafficItem.trafficItemDescriptionElement?.get(0)?.value.toString()
+
+        }
+
+    }
+
+}
