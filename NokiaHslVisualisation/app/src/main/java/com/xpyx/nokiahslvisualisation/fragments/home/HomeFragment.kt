49cdb4d194bc6b7a9748fae920096d316182ec2e
xpyx <pystynen@gmail.com>
2021-04-11 15:11:35 +0300

    remove unfinished method

--- NokiaHslVisualisation/app/src/main/java/com/xpyx/nokiahslvisualisation/fragments/home/HomeFragment.kt
+++ NokiaHslVisualisation/app/src/main/java/com/xpyx/nokiahslvisualisation/fragments/home/HomeFragment.kt
@@ -57,12 +57,12 @@ class HomeFragment : Fragment() {
     private val trafficIdRoomList = mutableListOf<Long>()
     private val trafficIdApiList = mutableListOf<Long>()
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.main_menu)
-        val item = menu.findItem(R.id.action_search)
-        return super.onCreateOptionsMenu(menu, inflater)
-    }
+//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+//        super.onCreateOptionsMenu(menu, inflater)
+//        inflater.inflate(R.menu.main_menu)
+//        val item = menu.findItem(R.id.action_search)
+//        return super.onCreateOptionsMenu(menu, inflater)
+//    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
