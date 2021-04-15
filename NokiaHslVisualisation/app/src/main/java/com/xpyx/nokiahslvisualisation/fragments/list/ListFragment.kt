06bf6e6b540f1e7ec34270992fc6012d14d4b410
Diakko <48632853+Diakko@users.noreply.github.com>
2021-04-15 13:16:35 +0300

    Messing with side navigation slider buttons

--- NokiaHslVisualisation/app/src/main/java/com/xpyx/nokiahslvisualisation/fragments/list/ListFragment.kt
+++ NokiaHslVisualisation/app/src/main/java/com/xpyx/nokiahslvisualisation/fragments/list/ListFragment.kt
@@ -1,18 +1,18 @@
 
 package com.xpyx.nokiahslvisualisation.fragments.list
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.navigation.NavigationView
 import com.xpyx.nokiahslvisualisation.R
+import com.xpyx.nokiahslvisualisation.R.*
 import com.xpyx.nokiahslvisualisation.data.TrafficItemViewModel
 import kotlinx.android.synthetic.main.radio_group_traffic_type.*
 
@@ -23,6 +23,7 @@ class ListFragment : Fragment() {
     private lateinit var mTrafficViewModel: TrafficItemViewModel
     private lateinit var viewHere: View
     private lateinit var filterSliderView: NavigationView
+    private val listOfFilters = mutableMapOf<String, Boolean>()
 
 
     override fun onCreateView(
@@ -30,7 +31,7 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        viewHere = inflater.inflate(R.layout.fragment_list, container, false)
+        viewHere = inflater.inflate(layout.fragment_list, container, false)
         filterSliderView = viewHere.findViewById(R.id.filter_drawer_view)
 
         val adapter = context?.let { TrafficListAdapter(requireContext()) }
@@ -50,20 +51,42 @@ class ListFragment : Fragment() {
         return viewHere
     }
 
+    @SuppressLint("LogNotTimber")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val menuItemCritical: MenuItem = filterSliderView.menu.findItem(R.id.critical_menu_item)
+        Log.d("MENUITEM", "${menuItemCritical.title}")
         filterSliderView.setNavigationItemSelectedListener { menuItem ->
+            if (menuItem.isChecked) {
+                Log.d("MENUCHECKED", "${menuItem.title}: ${menuItem.isChecked}")
+            }
+
+
             when (menuItem.itemId) {
 
                 // This is how to clear radio buttons. Radio groups .clearCheck() not working.
                 R.id.clear_button_type -> {
+                    Log.d("RADIOMENU","${incident_menu_item.isChecked}")
+                    Log.d("RADIOMENU","${event_menu_item.isChecked}")
                     incident_menu_item.isChecked = false
                     event_menu_item.isChecked = false
                     true
                 }
+
+                R.id.critical_menu_item -> {
+                    Log.d("Menu", "${menuItem.title}")
+                    true
+                }
+
                 R.id.apply_button_traffic_filters -> {
                     Log.d("BUTTON_TEST","TESTINGGG")
+
+                    listOfFilters["Critical"] = menuItem.isChecked
+                    Log.d("MENU", "Critical: ${menuItem.isChecked}")
+                    Log.d("MENU", "FILTERLIST: $listOfFilters")
+
+
                     true
                 }
                 else -> true
@@ -73,3 +96,4 @@ class ListFragment : Fragment() {
 
     }
 }
+
