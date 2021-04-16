1504b67d9b6aa8d0e2b52ea1248b9085f010387a
xpyx <pystynen@gmail.com>
2021-04-16 12:07:34 +0300

    remove realtime api

--- Realtime-Transport-Updates-API/src/data/queries/getAllStops.sql
+++
@@ -1,6 +0,0 @@
-SELECT stop_id
-    , stop_index
-    , stop_name
-    , stop_lat
-    , stop_lon
-FROM [dbo].[stops]
--- Realtime-Transport-Updates-API/src/data/queries/getTransfersFromStopId.sql
+++
@@ -1,6 +0,0 @@
-SELECT from_stop_id
-    , to_stop_id
-    , transfer_type
-    , min_transfer_time
-FROM [dbo].[transfers]
-WHERE from_stop_id = @stopId
