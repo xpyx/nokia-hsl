2632ba00e687be141a7a45c082318b43112b0485
xpyx <pystynen@gmail.com>
2021-04-28 13:55:47 +0300

    analytics layout ok

--- NokiaHslVisualisation/app/src/main/java/com/xpyx/nokiahslvisualisation/fragments/analytics/AnalyticsFragment.kt
+++ NokiaHslVisualisation/app/src/main/java/com/xpyx/nokiahslvisualisation/fragments/analytics/AnalyticsFragment.kt
@@ -9,7 +9,13 @@ import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.apollographql.apollo.api.Response
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartView
+import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
 import com.xpyx.nokiahslvisualisation.R
+import com.xpyx.nokiahslvisualisation.StopTimesListQuery
 import com.xpyx.nokiahslvisualisation.api.StopTimesViewModel
 import com.xpyx.nokiahslvisualisation.api.StopTimesViewModelFactory
 import com.xpyx.nokiahslvisualisation.repository.StopTimesRepository
@@ -35,6 +41,12 @@ class AnalyticsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        var responseForChart: Response<StopTimesListQuery.Data>? = null
+
+        // Init and hide chart
+        val aaChartView = view.findViewById<AAChartView>(R.id.aa_chart_view)
+        aaChartView.visibility = View.GONE
+
         // Init StopTimes API viewmodel
         val stopTimesRepository = StopTimesRepository()
         val stopTimesViewModelFactory = StopTimesViewModelFactory(stopTimesRepository)
@@ -59,8 +71,9 @@ class AnalyticsFragment : Fragment() {
                 val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss.SSS")
                 val formatted = current.format(formatter)
 
-
+                responseForChart = response
                 if (response != null) {
+
 
                     // Hide spinner
                     spinner.visibility = View.GONE
@@ -72,6 +85,7 @@ class AnalyticsFragment : Fragment() {
                         Query elapsed $diffInMillisec milliseconds
                         
                         """.trimIndent()
+
 
                     // The stoptimes data is here, iterate over the whole response
                     response.data?.stops()?.forEach {
@@ -119,10 +133,54 @@ class AnalyticsFragment : Fragment() {
 
                         }
                     }
+
+
+
+                    // Do chart magic
+                    val aaChartModel : AAChartModel = AAChartModel()
+                        .chartType(AAChartType.Area)
+                        .title("title")
+                        .subtitle("subtitle")
+                        .backgroundColor(R.color.design_default_color_background)
+                        .dataLabelsEnabled(true)
+                        .series(arrayOf(
+                            AASeriesElement()
+                                .name("Tokyo")
+                                .data(arrayOf(7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6)),
+                            AASeriesElement()
+                                .name("NewYork")
+                                .data(arrayOf(0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5)),
+                            AASeriesElement()
+                                .name("London")
+                                .data(arrayOf(0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0)),
+                            AASeriesElement()
+                                .name("Berlin")
+                                .data(arrayOf(3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8))
+                        )
+                        )
+
+                    //The chart view object calls the instance object of AAChartModel and draws the final graphic
+                    aaChartView.aa_drawChartWithChartModel(aaChartModel)
+
+                    // Show chart
+                    aaChartView.visibility = View.VISIBLE
+
+
+
+
                 } else {
                     // Log.d("DBG", response.toString())
                 }
             })
+
+
+
+
+
+
+
+
+
 
 
 
